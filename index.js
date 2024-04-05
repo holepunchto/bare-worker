db1c581bd3bff85d7b88993203ea1ba7a67d3d2f
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-04-05 15:40:20 +0200

    Progress

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* global Bare */
 const Channel = require('bare-channel')
 const MessagePort = require('./lib/message-port')
+const constants = require('./lib/constants')
 const { Thread } = Bare
 
 module.exports = exports = class Worker extends MessagePort {
@@ -11,7 +12,9 @@ module.exports = exports = class Worker extends MessagePort {
 
     super(channel.connect())
 
+    this._state = 0
     this._channel = channel
+    this._terminating = null
 
     this._thread = new Thread(require.resolve('./lib/thread'), {
       data: {
@@ -19,6 +22,37 @@ module.exports = exports = class Worker extends MessagePort {
         filename
       }
     })
+  }
+
+  terminate () {
+    if (this._state & constants.state.EXITED) return
+
+    if (this._terminating) return this._terminating
+
+    this._state |= constants.state.TERMINATING
+    this._terminating = this._terminate()
+
+    return this._terminating
+  }
+
+  async _terminate () {
+    await this._port.write({ type: constants.message.TERMINATE })
+  }
+
+  _ononline () {
+    this._state |= constants.state.ONLINE
+    this.emit('online')
+  }
+
+  async _onexit (exitCode) {
+    await this._port.close()
+
+    this._state |= constants.state.EXITED
+    this.emit('exit', exitCode)
+  }
+
+  _onerror (err) {
+    this.emit('error', err)
   }
 }
 
--- test.js
+++ test.js
@@ -2,17 +2,14 @@ const test = require('brittle')
 const Worker = require('.')
 
 test('basic', (t) => {
-  t.plan(2)
+  t.plan(3)
 
   const worker = new Worker(require.resolve('./test/fixtures/hello'))
 
   worker
-    .on('message', (message) => {
-      t.is(message, 'Hello worker')
-    })
-    .on('exit', (exitCode) => {
-      t.is(exitCode, 0)
-    })
+    .on('online', () => t.pass())
+    .on('message', (message) => t.is(message, 'Hello worker'))
+    .on('exit', (exitCode) => t.is(exitCode, 0))
 })
 
 test('uncaught exception', (t) => {
@@ -21,12 +18,8 @@ test('uncaught exception', (t) => {
   const worker = new Worker(require.resolve('./test/fixtures/throw'))
 
   worker
-    .on('error', (err) => {
-      t.is(err.message, 'error')
-    })
-    .on('exit', (exitCode) => {
-      t.is(exitCode, 1)
-    })
+    .on('error', (err) => t.is(err.message, 'error'))
+    .on('exit', (exitCode) => t.is(exitCode, 1))
 })
 
 test('exception with uncaught handler', (t) => {
@@ -35,10 +28,6 @@ test('exception with uncaught handler', (t) => {
   const worker = new Worker(require.resolve('./test/fixtures/throw-with-uncaught-handler'))
 
   worker
-    .on('message', (err) => {
-      t.is(err.message, 'error')
-    })
-    .on('exit', (exitCode) => {
-      t.is(exitCode, 0)
-    })
+    .on('message', (err) => t.is(err.message, 'error'))
+    .on('exit', (exitCode) => t.is(exitCode, 0))
 })
