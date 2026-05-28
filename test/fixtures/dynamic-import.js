9b7f38ba11392ad68445b65eb81d013b2c8f6b60
Yasser Nascimento <dev@yasser.email>
2026-05-28 09:07:32 -0300

    Simplify test

--- test/fixtures/dynamic-import.js
+++ test/fixtures/dynamic-import.js
@@ -1,10 +1,7 @@
 const { parentPort } = require('bare-worker')
 
-const importFn = new Function('path', 'return import(path)')
-
 async function main() {
-  const { default: data } = await importFn('./dynamic-import-return.js')
-
+  const { default: data } = await import('./dynamic-import-return.js')
   parentPort.postMessage(data)
 }
 
